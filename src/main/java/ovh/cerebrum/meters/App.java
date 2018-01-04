9dceffa90938724a79500df96c73058858d6113a
heimdall <hemidal@gmail.com>
2018-01-04 22:07:15 +0100

    #6

--- src/main/java/ovh/cerebrum/meters/App.java
+++ src/main/java/ovh/cerebrum/meters/App.java
@@ -1,12 +1,10 @@
 package ovh.cerebrum.meters;
 
-import com.coreoz.plume.conf.dagger.DaggerConfModule;
-import dagger.Component;
-
 import static spark.Spark.get;
 
-@Component(modules = DaggerConfModule.class)
 public class App {
+
+
 
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello world");
